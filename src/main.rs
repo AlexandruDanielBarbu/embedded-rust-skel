f33c020601ead8b0be66b612149001c6a720a63a
AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>
2026-04-01 19:07:43 +0300

    Did lab 4 task 1. All works fine
    Signed-off-by: AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
 #![no_std]
 #![no_main]
 
-use cortex_m as _;
-use cortex_m_rt::entry;
-use defmt::{error, info};
-// use RTT for defmt transport
-use core::panic::PanicInfo;
+use defmt::info;
 use defmt_rtt as _;
+use embassy_executor::{Spawner, task};
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::{Instant, Timer};
+use panic_probe as _;
 
-#[entry]
-fn main() -> ! {
-    info!("This is my custom message. :)))))))))))))))))");
-    // panic!("panic here");
+fn busy_wait(ms: u64) {
+    let start_time = Instant::now();
+    while start_time.elapsed().as_millis() < ms {}
+}
+
+#[task(pool_size = 2)]
+async fn led_blink(mut led_pin: Output<'static>) {
     loop {
-        // Your code goes here
+        led_pin.set_low();
+        // busy_wait(500);
+        Timer::after_millis(500).await;
 
-        // For now, we just spin to satisfy the "!" return type
-        cortex_m::asm::nop();
+        led_pin.set_high();
+        // busy_wait(500);
+        Timer::after_millis(500).await;
     }
 }
 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    error!("{:?}", info);
-    loop {}
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let peripherals = embassy_stm32::init(Default::default());
+    info!("Device started Boyyyy");
+
+    // The red LED is connected to D8 (PC7).
+    let led_red = Output::new(peripherals.PC7, Level::High, Speed::Low);
+    // The blue LED is connected to D9 (PC6).
+    let led_blue = Output::new(peripherals.PC6, Level::High, Speed::Low);
+
+    spawner.spawn(led_blink(led_red)).unwrap();
+    spawner.spawn(led_blink(led_blue)).unwrap();
 }

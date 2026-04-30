90904971b2df81590c9aa4782d0ff08fa09c9909
AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>
2026-04-30 16:59:22 +0300

    S-a rezolvat!
    Signed-off-by: AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,91 +3,41 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::i2c::I2c;
-use embassy_stm32::{bind_interrupts, i2c, peripherals};
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
 use embassy_time::{Duration, Timer, with_timeout};
 use {defmt_rtt as _, panic_probe as _};
-
-bind_interrupts!(struct Irqs {
-    I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
-    I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
-});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
-    let mut config = i2c::Config::default();
-    config.sda_pullup = true;
-    config.scl_pullup = true;
-
-    let mut i2c = I2c::new(
-        p.I2C1,
-        p.PB6, // SCL
-        p.PB7, // SDA
-        Irqs,
-        p.GPDMA1_CH0,
-        p.GPDMA1_CH1,
-        config,
-    );
-
-    info!("Incep I2C scan...");
-
-    for addr in 0x08u8..=0x77 {
-        let mut buf = [0u8; 1];
-        match with_timeout(Duration::from_millis(10), i2c.read(addr, &mut buf)).await {
-            Ok(Ok(_)) => info!("Gasit dispozitiv la adresa: 0x{:02X}", addr),
-            _ => {}
-        }
-    }
-
-    info!("Scan complet.");
-
-    // ------------------------- EX 2
-
-    // Setează adresa senzorului (schimbă în 0x76 dacă e nevoie)
-    const BMP390_ADDR: u8 = 0x76;
-
-    info!("Configurare BMP390...");
-
-    // 1. Configurare OSR (0x1C): Supradeșantionare temperatură x2
-    // Trimitem un array cu adresa registrului și valoarea dorită
-    if let Err(_) = i2c.write(BMP390_ADDR, &[0x1C, 0b00_001_000]).await {
-        error!("Eroare la scrierea in registrul OSR");
-    }
-
-    // 2. Configurare PWR_CTRL (0x1B): Mod Normal, Temp ON, Pres OFF
-    if let Err(_) = i2c.write(BMP390_ADDR, &[0x1B, 0b00_11_00_10]).await {
-        error!("Eroare la scrierea in registrul PWR_CTRL");
-    }
-
-    info!("Senzor configurat! Incepem citirea la fiecare secunda...");
+    let mut red_led = Output::new(p.PA5, Level::Low, Speed::Medium);
+    let mut yellow_led = Output::new(p.PA6, Level::Low, Speed::Medium);
+    let mut green_led = Output::new(p.PA7, Level::Low, Speed::Medium);
+    let switch = Input::new(p.PC9, Pull::Up);
 
     loop {
-        // Vrem să citim 3 bytes începând cu DATA_3 (Adresa 0x07)
-        let mut temp_data = [0u8; 3];
+        // Init
+        green_led.set_low();
+        yellow_led.set_high();
+        red_led.set_high();
 
-        // write_read trimite adresa registrului de start (0x07), apoi citește 3 bytes direct în temp_data
-        match i2c.write_read(BMP390_ADDR, &[0x07], &mut temp_data).await {
-            Ok(_) => {
-                // temp_data[0] = DATA_3 (XLSB)
-                // temp_data[1] = DATA_4 (LSB)
-                // temp_data[2] = DATA_5 (MSB)
+        // logic
+        if switch.is_low() {
+            info!("S-a apasat butonul!!!");
+            Timer::after_secs(2).await;
 
-                // Asamblăm cele 3 bucăți de 8 biți într-un număr de 24 de biți (u32)
-                let raw_temp: u32 = ((temp_data[2] as u32) << 16)
-                    | ((temp_data[1] as u32) << 8)
-                    | (temp_data[0] as u32);
+            green_led.set_high();
+            yellow_led.set_low();
 
-                info!("Valoare Raw Temperatura: {}", raw_temp);
-            }
-            Err(e) => {
-                // {:?} forteaza printarea detaliilor tehnice ale erorii (ex: Timeout, Nack, Overrun)
-                error!("A esuat citirea! Motiv: {:?}", e);
-            }
+            Timer::after_secs(1).await;
+
+            yellow_led.set_high();
+            red_led.set_low();
+
+            Timer::after_secs(3).await;
+        } else {
+            Timer::after_secs(1).await;
         }
-
-        // Așteptăm 1 secundă
-        Timer::after_secs(1).await;
     }
 }

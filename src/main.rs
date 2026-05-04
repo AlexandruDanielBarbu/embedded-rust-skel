ac858c4d1e3c1b3108e471ff6dcdbf1775888a6b
AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>
2026-05-04 13:07:10 +0300

    PWM pe servomotor merge, dar scartaie motorul. Presupun ca este de la valorile duty cycle.
    Signed-off-by: AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use defmt::info;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_stm32::{
+    crc::PolySize,
     gpio::{Input, Level, Output, OutputType, Pull, Speed},
-    time::khz,
+    time::{hz, khz},
     timer::simple_pwm::{PwmPin, SimplePwm},
 };
 use embassy_time::Timer;
@@ -16,28 +17,44 @@ use panic_probe as _;
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
-    let mut led_pin = PwmPin::new(p.PA0, OutputType::PushPull);
+    let servo_pin = PwmPin::new(p.PA0, OutputType::PushPull);
 
     let mut pwm = SimplePwm::new(
         p.TIM2,
-        Some(led_pin),
+        Some(servo_pin),
         None,
         None,
         None,
-        khz(1),
+        hz(50),
         Default::default(),
     );
 
-    info!("PWM started!");
+    info!("PWM started for servo!");
 
     let mut ch1 = pwm.ch1();
     ch1.enable();
 
-    loop {
-        // ch1.set_duty_cycle_percent(50);
-        ch1.set_duty_cycle_percent(10);
-        // ch1.set_duty_cycle_percent(100);
+    const MIN_PERIOD_US: u32 = 500;
+    const MAX_PERIOD_US: u32 = 2500;
+    const PERIOD_US: u32 = 20000;
 
-        Timer::after_millis(10).await;
+    let min_value = (MIN_PERIOD_US * 1000) / PERIOD_US;
+    let max_value = (MAX_PERIOD_US * 1000) / PERIOD_US;
+
+    let WAIT = 100;
+    loop {
+        for pos in min_value..=max_value {
+            ch1.set_duty_cycle_fraction(pos as u16, 1000);
+
+            Timer::after_millis(WAIT).await;
+        }
+
+        for pos in (min_value..=max_value).rev() {
+            ch1.set_duty_cycle_fraction(pos as u16, 1000);
+
+            Timer::after_millis(WAIT).await;
+        }
+
+        Timer::after_millis(WAIT).await;
     }
 }

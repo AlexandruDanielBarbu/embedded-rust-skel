a13f345264220b22a13b7cf3e92db7eb6d26eed5
AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>
2026-05-04 12:46:45 +0300

    LED pwm merge bine
    Signed-off-by: AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,39 @@
 use defmt::info;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embassy_stm32::{
+    gpio::{Input, Level, Output, OutputType, Pull, Speed},
+    time::khz,
+    timer::simple_pwm::{PwmPin, SimplePwm},
+};
 use embassy_time::Timer;
 use panic_probe as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let peripherals = embassy_stm32::init(Default::default());
+    let p = embassy_stm32::init(Default::default());
 
-    let mut led = Output::new(peripherals.PC7, Level::Low, Speed::Low);
-    let button = Input::new(peripherals.PA8, Pull::Up);
+    let mut led_pin = PwmPin::new(p.PA0, OutputType::PushPull);
 
-    info!("Dummy started!");
+    let mut pwm = SimplePwm::new(
+        p.TIM2,
+        Some(led_pin),
+        None,
+        None,
+        None,
+        khz(1),
+        Default::default(),
+    );
+
+    info!("PWM started!");
+
+    let mut ch1 = pwm.ch1();
+    ch1.enable();
 
     loop {
-        if button.is_low() {
-            led.set_high();
-        } else {
-            led.set_low();
-        }
+        // ch1.set_duty_cycle_percent(50);
+        ch1.set_duty_cycle_percent(10);
+        // ch1.set_duty_cycle_percent(100);
 
         Timer::after_millis(10).await;
     }
